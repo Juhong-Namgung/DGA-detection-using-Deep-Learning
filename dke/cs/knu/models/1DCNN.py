b03b650f86bb57f2c35c08d7c0c33c119c60acec
Juhong Namgung <wnghd9999@naver.com>
2020-01-04 01:01:59 +0900

    Fix CNN model to simplify code

--- dke/cs/knu/models/1DCNN.py
+++ dke/cs/knu/models/1DCNN.py
@@ -1,71 +1,25 @@
 # Load Libraries
-import pandas as pd
-import numpy as np
-import re, os
-from string import printable
-from sklearn import model_selection
-
-import tensorflow as tf
-from keras.models import Sequential, Model, model_from_json, load_model
-from keras import regularizers
-from keras.layers.core import Dense, Dropout, Activation, Lambda, Flatten
-from keras.layers import Input, ELU, LSTM, Embedding, Convolution2D, MaxPooling2D, \
-    BatchNormalization, Convolution1D, MaxPooling1D, concatenate
-from keras.preprocessing import sequence
-from keras.optimizers import SGD, Adam, RMSprop
-from keras.utils import np_utils
-from keras import backend as K
-from sklearn.model_selection import KFold
-from pathlib import Path
-from keras.utils.vis_utils import model_to_dot
-from IPython.display import SVG
-from keras.utils import plot_model
-from tensorflow.python.platform import gfile
-
-import json
 
 import warnings
+
+import model_evaluate
+import model_preproecess
+import tensorflow as tf
+from keras import backend as K
+from keras import regularizers
+from keras.layers import Input, ELU, Embedding, BatchNormalization, Convolution1D, MaxPooling1D, concatenate
+from keras.layers.core import Dense, Dropout, Lambda
+from keras.models import Model
+from keras.optimizers import Adam
 
 warnings.filterwarnings("ignore")
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.9
 K.tensorflow_backend.set_session(tf.Session(config=config))
 
-# General save model to disk function
-def save_model(fileModelJSON, fileWeights):
-    if Path(fileModelJSON).is_file():
-        os.remove(fileModelJSON)
-    json_string = model.to_json()
-    with open(fileModelJSON, 'w') as f:
-        json.dump(json_string, f)
-    if Path(fileWeights).is_file():
-        os.remove(fileWeights)
-    model.save_weights(fileWeights)
-
 with tf.device("/GPU:0"):
 
-    # Load data
-    DATA_HOME ='../data/'
-    #DATA_HOME ='/home/jhnamgung/kcyber/data/'
-    df = pd.read_csv(DATA_HOME + 'dga_01_train.csv',encoding='ISO-8859-1', sep=',')
-
-    # Convert domain string to integer
-    # URL 알파벳을 숫자로 변경
-    url_int_tokens = [[printable.index(x) + 1 for x in url if x in printable] for url in df.domain]
-
-    # Padding domain integer max_len=74
-    # 최대길이 74로 지정
-    max_len = 74
-
-    X = sequence.pad_sequences(url_int_tokens, maxlen=max_len)
-    y = np.array(df['class'])
-
-    # Cross-validation
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=33)
-
-with tf.device("/GPU:0"):
-
-    def conv_fully(max_len=74, emb_dim=32, max_vocab_len=100, W_reg=regularizers.l2(1e-4)):
+    def conv_fully(max_len=77, emb_dim=32, max_vocab_len=128, W_reg=regularizers.l2(1e-4)):
 
         # Input
         main_input = Input(shape=(max_len,), dtype='int32', name='main_input')
@@ -104,45 +58,49 @@ with tf.device("/GPU:0"):
         hidden1 = BatchNormalization(mode=0)(hidden1)
         hidden1 = Dropout(0.5)(hidden1)
 
-        hidden2 = Dense(1024)(hidden1)
+        hidden2 = Dense(256)(hidden1)
         hidden2 = ELU()(hidden2)
         hidden2 = BatchNormalization(mode=0)(hidden2)
         hidden2 = Dropout(0.5)(hidden2)
 
-        hidden3 = Dense(256)(hidden2)
+        hidden3 = Dense(64)(hidden2)
         hidden3 = ELU()(hidden3)
         hidden3 = BatchNormalization(mode=0)(hidden3)
         hidden3 = Dropout(0.5)(hidden3)
 
-        hidden4 = Dense(64)(hidden3)
-        hidden4 = ELU()(hidden4)
-        hidden4 = BatchNormalization(mode=0)(hidden4)
-        hidden4 = Dropout(0.5)(hidden4)
-
         # Output layer (last fully connected layer)
         # 마지막 클래스 결정하는 layer
-        output = Dense(1, activation='sigmoid', name='main_output')(hidden4)
+        output = Dense(21, activation='softmax', name='main_output')(hidden3)
 
         # Compile model and define optimizer
         model = Model(input=[main_input], output=[output])
         adam = Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy', tf.keras.metrics.CategoricalAccuracy(), preprocess.precision, preprocess.recall, preprocess.fmeasure])
         return model
 
 with tf.device("/GPU:0"):
     epochs = 10
-    batch_size = 16
+    batch_size = 64
 
+    # Load data using model preprocessor
+    preprocess = model_preproecess.Preprocessor()
 
+    X_train, X_test, y_train, y_test = preprocess.load_data()
 
-
+    # define CNN model
     model = conv_fully()
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
-    loss, accuracy = model.evaluate(X_test, y_test, verbose=1)
-    print('\nFinal Cross-Validation Accuracy', accuracy, '\n')
+    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.11)
+
+    evaluator = model_evaluate.Evaluator()
+
+    # validation curves
+    #evaluator.plot_validation_curves(history)
+
+    # experimental result
+    evaluator.calculate_measrue(model, X_test, y_test)
+
+    # model.summary()
 
     # # Save final training model
     # model_name = "1DCNN"
     # save_model("../models/" + model_name + ".json", "../models/" + model_name + ".h5")
-    model.summary()
-
